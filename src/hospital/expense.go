bf96107c76a9f188440fedf7208c6839e4ff0187
zhenchao.bi <zhenchao.bi@accenture.com>
2017-06-22 15:23:24 +0800

    add

--- src/hospital/expense.go
+++ src/hospital/expense.go
@@ -6,32 +6,68 @@ _ "strconv"
 	_ "encoding/json"
 "github.com/hyperledger/fabric/core/chaincode/shim"
 pb "github.com/hyperledger/fabric/protos/peer"
+	"encoding/json"
+	"encoding/binary"
 )
 
 type MedicineDetail struct {
 	Id string
 	Name string
 	Price int
-	number int
+	Number int
 }
 
 type ExpenseDetail struct {
 	Uid string
-	ExpenseTime int64
+	//yyyyMMddHHmmss
+	ExpenseTime string
 	Claimed bool
 	Medicines []MedicineDetail
 }
 
 
 type HospitalChainCode struct {
-
 }
 
 func (t *HospitalChainCode) Init(stub shim.ChaincodeStubInterface) pb.Response {
-	return nil
+	return shim.Success(nil)
 }
 
 func (t *HospitalChainCode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+	function, args := stub.GetFunctionAndParameters()
+	if function == "invoke" {
+		return t.invoke(stub, args)
+	} else if function == "query" {
+		return t.query(stub, args)
+	}
+	return shim.Error(`invalid invoke function name: "invoke" "query"`)
+}
+
+func (t *HospitalChainCode) invoke(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) != 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
+	}
+
+	jsonVal := args[0]
+
+	var jsonObj ExpenseDetail
+	err := json.Unmarshal([]byte(jsonVal), &jsonObj)
+	if err != nil {
+		return shim.Error("Fail to unmarshal json data!")
+	}
+
+	// usrMapdataBytes is []byte
+	usrMapdataBytes, err := stub.GetState(jsonObj.Uid)
+
+
+	// map is not found
+	if err != nil {
+	//	usrMapdata := map[string][]ExpenseDetail{}
+}
+	usrMapdata[jsonObj.ExpenseTime] := []byte(jsonObj)
+
+
+
 	return nil
 }
 
