9117127586aa0c66c68067923291384c535419c2
bizhenchao1201 <bizhenchao1201@hotmail.com>
2017-06-22 13:22:13 +0800

    add chaincode

--- src/hospital/expense.go
+++ src/hospital/expense.go
@@ -3,14 +3,27 @@ package main
 import (
 "fmt"
 _ "strconv"
-
+	_ "encoding/json"
 "github.com/hyperledger/fabric/core/chaincode/shim"
 pb "github.com/hyperledger/fabric/protos/peer"
 )
 
+type MedicineDetail struct {
+	Id string
+	Name string
+	Price int
+	number int
+}
+
+type ExpenseDetail struct {
+	Uid string
+	ExpenseTime int64
+	Claimed bool
+	Medicines []MedicineDetail
+}
+
+
 type HospitalChainCode struct {
-	Uid string `"json: user_id"`
-	ExpenseList map[string]string
 
 }
 
