f07f39075cc5a2d736c9b1d04317a19bcf0765f2
bizhenchao1201 <bizhenchao1201@hotmail.com>
2017-06-23 11:39:55 +0800

    add chaincode

--- src/hospital/expense.go
+++ src/hospital/expense.go
@@ -58,10 +58,10 @@ func (t *HospitalChainCode) invoke(stub shim.ChaincodeStubInterface, args []stri
 		return shim.Error("Fail to unmarshal json data!")
 	}
 
+	 usrMapdata := map[string]ExpenseDetail{}
+
 	// usrMapdataBytes is []byte
 	usrMapdataBytes, err := stub.GetState(jsonObj.Uid)
-
-	var usrMapdata map[string]ExpenseDetail
 	// map is not found
 	if err == nil {
 		buf := new(bytes.Buffer)
@@ -73,7 +73,16 @@ func (t *HospitalChainCode) invoke(stub shim.ChaincodeStubInterface, args []stri
 	buf := new(bytes.Buffer)
 	binary.Write(buf, binary.BigEndian, usrMapdata)
 
-	stub.PutState(jsonObj.Uid, buf.Bytes())
+	stub.PutState(jsonObj.Uid,[]byte("bizhenchaoTest"))
+	//stub.PutState(jsonObj.Uid, buf.Bytes())
+
+/////////////////////////
+	usrMapdataBytes22, err := stub.GetState(jsonObj.Uid)
+	if err != nil {
+		return shim.Success([]byte(jsonObj.Uid))
+	}
+
+	fmt.Printf("JSON data is %s", string(usrMapdataBytes22))
 
 	return shim.Success(nil)
 }
@@ -81,7 +90,7 @@ func (t *HospitalChainCode) invoke(stub shim.ChaincodeStubInterface, args []stri
 func (t *HospitalChainCode) query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 
 	if len(args) != 1 {
-		shim.Error("Incorrect number of arguments. Expecting 1 -> user id")
+		return shim.Error("Incorrect number of arguments. Expecting 1 -> user id")
 	}
 
 	uid := args[0]
@@ -90,7 +99,7 @@ func (t *HospitalChainCode) query(stub shim.ChaincodeStubInterface, args []strin
 		return shim.Success([]byte(uid))
 	}
 
-	var usrMapdata map[string]ExpenseDetail
+	usrMapdata := map[string]ExpenseDetail{}
 	buf := new(bytes.Buffer)
 	buf.Write(usrMapdataBytes)
 	binary.Read(buf, binary.BigEndian, usrMapdata)
