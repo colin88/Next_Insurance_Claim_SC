6bd4d5367014e9207894fee922845c59f1f49e36
rizzo88 <colin1030>
2017-06-25 21:27:41 -0700

    add customer chaincode including chaincode invoke chaincode

---
+++ src/customer/Customer_test.go
@@ -0,0 +1,31 @@
+package main
+
+//noinspection GoUnresolvedReference
+import (
+	"testing"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	_ "fmt"
+	"github.com/hyperledger/fabric/common/util"
+	hosp "InsuranceClaim/Next_Insurance_Claim_SC/src/hospital"
+	"fmt"
+)
+
+func TestCustomerChainCode_Init(t *testing.T) {//stub *shim.MockStub, args [][]byte
+	scc := new(CustomerChainCode)
+	stub := shim.NewMockStub("test1", scc)
+
+	chaincodeToInvoke := "hosp01"
+	cc2 := new(hosp.HospitalChainCode)
+	stubExec2 := shim.NewMockStub(chaincodeToInvoke, cc2)
+
+	stub.MockPeerChaincode(chaincodeToInvoke, stubExec2)
+
+	f := "invoke"
+	args := util.ToChaincodeArgs(f, chaincodeToInvoke, "", "3702821982")
+	res := stub.MockInvoke("01", args)
+	if res.Status != shim.OK {
+		fmt.Println("------Invoker Hospital info failed", string(res.Message))
+		t.FailNow()
+	}
+
+}
